b2b327736709a7f37377135bb042b92fc98c6339
1nteger <rhep0820@gmail.com>
2019-08-08 01:19:35 +0900

    refactor : EntityManager의 CRUD 예시 코드 수정

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,37 +17,33 @@ public class JpaMain {
 
         try {
             // 삽입
-            /*Member member = new Member();
+            Member member = new Member();
             member.setId(1L);
             member.setName("HelloA");
-            entityManager.persist(member);*/
+            entityManager.persist(member);
 
             // 조회
-            /*Member findMember = entityManager.find(Member.class, 1L);
+            Member findMember = entityManager.find(Member.class, 1L);
             System.out.println("findMember.id = " + findMember.getId());
-            System.out.println("findMember.name = " + findMember.getName());*/
+            System.out.println("findMember.name = " + findMember.getName());
 
             // 삭제
-            /*Member findMember = entityManager.find(Member.class, 1L);
-            entityManager.remove(findMember);*/
+            entityManager.remove(findMember);
 
             // 수정
-            /*Member findMember = entityManager.find(Member.class, 1L);
-            findMember.setName("changedName");*/
+            findMember.setName("changedName");
 
             // JPQL
-            List<Member> result = entityManager.createQuery("select m from Member as m", Member.class)
+            List<Member> results = entityManager.createQuery("select m from Member as m", Member.class)
                     .setFirstResult(1)
                     .setMaxResults(5) // 이렇게 사용하면 간편하게 페이징 처리가 된다. (방언에 따라 페이징 쿼리가 나간다)
                     .getResultList(); // JPA는 절대 테이블을 대상으로 코드를 짜지 않는다. 테이블이 아니라 객체가 대상이 된다.
 
-            for (Member member : result) {
-                System.out.println("member.name = " + member.getName());
+            for (Member result : results) {
+                System.out.println("member.name = " + result.getName());
             }
 
-
             transaction.commit();
-
         } catch (Exception e) {
             transaction.rollback();
         } finally {
