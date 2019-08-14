c108a7958006748429ba598806832c26a723211e
한정수 <integer@zuminternet.com>
2019-08-14 09:51:21 +0900

    연관관계 ì의 주인 내용 추가

--- src/main/java/relationalMapping/Team.java
+++ src/main/java/relationalMapping/Team.java
@@ -19,6 +19,6 @@ public class Team {
 
     private String name;
 
-    @OneToMany(mappedBy = "team")
+    @OneToMany(mappedBy = "team") // 연관관계 주인인 team에 의해 관리되므로 여기에 값을 넣거나 업데이트해도 소용없다. 다만 조회는 가능하다.
     private List<Member> members = new ArrayList<>(); // 이렇게 ArrayList로 초기화 하면 add 할 때 NPE를 방지할 수 있으므로 관례적으로 사용한다.
 }
